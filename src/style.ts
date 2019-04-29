5858cf5a31aa9bfc468c8ba020b2fbe308e99900
Matthew Wagerfield <matthew@wagerfield.com>
2019-04-29 16:35:54 +0100

    style: convert nested style function to arrow

--- src/style.ts
+++ src/style.ts
@@ -47,30 +47,28 @@ export const style = ({
   themeKeys,
   transform,
   fallback
-}: StyleOptions): StyleFunction => {
-  return <P extends Props>(props: P) => {
-    // Get first props value from propsKeys
-    let value = resolveValue(props, propsKeys)
+}: StyleOptions): StyleFunction => <P extends Props>(props: P) => {
+  // Get first props value from propsKeys
+  let value = resolveValue(props, propsKeys)
 
-    // Return null when value is undefined
-    if (isNil(value)) return null
+  // Return null when value is undefined
+  if (isNil(value)) return null
 
-    // Resolve themeKey from themeKeys
-    const themeKey = resolveKey(props, themeKeys)
+  // Resolve themeKey from themeKeys
+  const themeKey = resolveKey(props, themeKeys)
 
-    // // Resolve lookup from themeKey
-    const lookup = pathOr(fallback)(["theme", themeKey])(props)
+  // // Resolve lookup from themeKey
+  const lookup = pathOr(fallback)(["theme", themeKey])(props)
 
-    // Convert value to path
-    const path = toPath(value)
+  // Convert value to path
+  const path = toPath(value)
 
-    // Resolve value from lookup
-    value = pathOr(value)(path)(lookup)
+  // Resolve value from lookup
+  value = pathOr(value)(path)(lookup)
 
-    // Transform value
-    if (isTransform(transform)) value = transform(value)
+  // Transform value
+  if (isTransform(transform)) value = transform(value)
 
-    // Return style object
-    return createStyle(value, styleKeys)
-  }
+  // Return style object
+  return createStyle(value, styleKeys)
 }
