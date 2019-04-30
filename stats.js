fdf724e415d0029ebc6e7042590071247bf4629a
Matthew Wagerfield <matthew@wagerfield.com>
2019-04-30 13:24:34 +0100

    chore: refine stats logic

--- stats.js
+++ stats.js
@@ -1,11 +1,11 @@
 const gzipSize = require("gzip-size")
 const glob = require("glob")
 
-const getSize = (files) =>
-  files.reduce((total, path) => {
+const getStats = (pattern) => {
+  return glob.sync(pattern).reduce((total, path) => {
     return total + gzipSize.fileSync(path)
   }, 0)
+}
 
-glob("lib/*.js", {}, (error, files) => {
-  if (files) console.log(getSize(files))
-})
+console.log("themed-system:", getStats("lib/*.js"))
+console.log("styled-system:", getStats("tmp/styled-system.js"))
