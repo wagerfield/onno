37e8b4c5513623a31c4a7d44b667d402b698e0f1
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-04 14:25:39 +0100

    test: add fixture and format json

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -10,7 +10,35 @@ export const style = (options?: Partial<T.StyleOptions>) =>
   })
 
 export const snapshot = (func: T.Func) => (props: T.Props, label?: string) => {
-  let snapshotName = JSON.stringify(props)
+  let snapshotName = JSON.stringify(props, null, 2)
   if (label) snapshotName = `[${label}] ${snapshotName}`
   expect(func(props)).toMatchSnapshot(snapshotName)
+}
+
+export const fixture = {
+  foo: "FOO",
+  bar: {
+    a: "A",
+    b: 0,
+    c: false,
+    d: [11, 22, 33]
+  },
+  baz: [
+    {
+      alias: "A0" // No value
+    },
+    {
+      value: "V1" // No alias
+    },
+    {
+      alias: "A2",
+      value: "V2"
+    },
+    {
+      alias: "A3",
+      value: 0
+    },
+    "V4", // Normal value
+    0 // Falsey value
+  ]
 }
