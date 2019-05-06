f02a6ca7e7be76db5a8302316df4d6c8c5aafae4
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-06 13:13:07 +0100

    refactor: move snapshots to snap dir

--- snaps.js
+++ snaps.js
@@ -1,14 +1,19 @@
 // https://jestjs.io/docs/en/configuration#snapshotresolver-string
+
+const snapDir = "snap"
+const testDir = "test"
+const testExt = ".test.ts"
+
 module.exports = {
-  testPathForConsistencyCheck: "some/file.test.ts",
+  testPathForConsistencyCheck: "test/file.test.ts",
   // resolves from test to snapshot path
-  // some/file.test.ts > some/file.snap
+  // test/file.test.ts > snap/file.snap
   resolveSnapshotPath: (testPath, snapExt) => {
-    return testPath.replace(".test.ts", snapExt)
+    return testPath.replace(testDir, snapDir).replace(testExt, snapExt)
   },
   // resolves from snapshot to test path
-  // some/file.snap > some/file.test.ts
+  // snap/file.snap > test/file.test.ts
   resolveTestPath: (snapPath, snapExt) => {
-    return snapPath.replace(snapExt, ".test.ts")
+    return snapPath.replace(snapDir, testDir).replace(snapExt, testExt)
   }
 }
