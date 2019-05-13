f1b8a51593424edbd05800a69d08ef6da2d4b4d0
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-13 15:31:22 +0100

    feat: add size snapshots and prettier formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,29 @@
+import prettier from "rollup-plugin-prettier"
 import typescript from "rollup-plugin-typescript"
-import { resolve } from "path"
+import { sizeSnapshot } from "rollup-plugin-size-snapshot"
 import { sync } from "glob"
+import { join } from "path"
 
 export default sync("packages/*").map((pkg) => ({
-  input: resolve(pkg, "src/index.ts"),
+  input: join(pkg, "src/index.ts"),
   output: [
     {
-      file: resolve(pkg, "cjs/index.js"),
+      file: join(pkg, "cjs/index.js"),
       format: "cjs"
     },
     {
-      file: resolve(pkg, "esm/index.js"),
+      file: join(pkg, "esm/index.js"),
       format: "esm"
     }
   ],
-  plugins: [typescript()]
+  plugins: [
+    typescript(),
+    prettier({
+      parser: "babel"
+    }),
+    sizeSnapshot({
+      snapshotPath: "stats.json",
+      printInfo: false
+    })
+  ]
 }))
