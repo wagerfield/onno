605e70a7c7e6550338d5b16284392b5081cd656e
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-13 17:05:00 +0100

    refactor: snapshot resolver

--- snapshots.js
+++ snapshots.js
@@ -1,20 +1,14 @@
-const path = require("path")
-
-const snapDir = path.resolve("snaps")
-const testDir = path.resolve("tests")
-const testExt = ".test.ts"
-
 // https://jestjs.io/docs/en/configuration#snapshotresolver-string
 module.exports = {
   testPathForConsistencyCheck: "test/file.test.ts",
   // resolves from test to snapshot path
-  // test/file.test.ts > snap/file.snap
-  resolveSnapshotPath: (testPath, snapExt) => {
-    return testPath.replace(testDir, snapDir).replace(testExt, snapExt)
+  // test/file.test.ts > test/snapshots/file.snap
+  resolveSnapshotPath: (testPath) => {
+    return testPath.replace(/(\w+)\.test\.ts$/, "snapshots/$1.snap")
   },
   // resolves from snapshot to test path
-  // snap/file.snap > test/file.test.ts
-  resolveTestPath: (snapPath, snapExt) => {
-    return snapPath.replace(snapDir, testDir).replace(snapExt, testExt)
+  // test/snapshots/file.snap > test/file.test.ts
+  resolveTestPath: (snapPath) => {
+    return snapPath.replace(/snapshots\/(\w+)\.snap$/, "$1.test.ts")
   }
 }
