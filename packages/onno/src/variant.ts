09fc208de6a590f2989d4bc654cb8f062b40c525
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-25 07:33:05 +0100

    refactor: rename variant prop interfaces

--- packages/onno/src/variant.ts
+++ packages/onno/src/variant.ts
@@ -5,36 +5,36 @@ export type VariantProp = T.Prop<number | string>
 
 // Button
 
-export interface ButtonVariantProps extends T.ThemeProps {
+export interface ButtonStyleProps extends T.ThemeProps {
   buttonStyle?: VariantProp
   bst?: VariantProp
 }
 
-export const buttonStyle = variant<ButtonVariantProps>({
+export const buttonStyle = variant<ButtonStyleProps>({
   propsKeys: ["buttonStyle", "bst"],
   themeKeys: ["buttonStyles"]
 })
 
 // Color
 
-export interface ColorVariantProps extends T.ThemeProps {
+export interface ColorStyleProps extends T.ThemeProps {
   colorStyle?: VariantProp
   cst?: VariantProp
 }
 
-export const colorStyle = variant<ColorVariantProps>({
+export const colorStyle = variant<ColorStyleProps>({
   propsKeys: ["colorStyle", "cst"],
   themeKeys: ["colorStyles"]
 })
 
 // Text
 
-export interface TextVariantProps extends T.ThemeProps {
+export interface TextStyleProps extends T.ThemeProps {
   textStyle?: VariantProp
   tst?: VariantProp
 }
 
-export const textStyle = variant<TextVariantProps>({
+export const textStyle = variant<TextStyleProps>({
   propsKeys: ["textStyle", "tst"],
   themeKeys: ["textStyles"]
 })
