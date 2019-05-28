88bddaff8363d9b0206a9c0e151e94495fb06b0e
Matthew Wagerfield <matthew@wagerfield.com>
2019-05-28 15:50:34 +0100

    refactor: refine global render functions

--- packages/onno/src/global.ts
+++ packages/onno/src/global.ts
@@ -1,34 +1,48 @@
 import { compose } from "./style"
+import {
+  border,
+  BorderProps,
+  BorderStyle,
+  borderRadius,
+  BorderRadiusProps,
+  BorderRadiusStyle
+} from "./border"
 import { transition, TransitionProps, TransitionStyle } from "./animation"
-import { borderSet, BorderSetProps, BorderSetStyle } from "./border"
 import { boxShadow, BoxShadowProps, BoxShadowStyle } from "./shadow"
 import { colorSet, ColorSetProps, ColorSetStyle } from "./color"
 import { spaceSet, SpaceSetProps, SpaceSetStyle } from "./space"
+import { opacity, OpacityProps, OpacityStyle } from "./display"
 import { sizeSet, SizeSetProps, SizeSetStyle } from "./layout"
 import { textSet, TextSetProps, TextSetStyle } from "./text"
 
 // Global Set
 
-export type GlobalSetProps = BorderSetProps &
+export type GlobalSetProps = BorderProps &
+  BorderRadiusProps &
   BoxShadowProps &
   ColorSetProps &
+  OpacityProps &
   SpaceSetProps &
   SizeSetProps &
   TextSetProps &
   TransitionProps
 
-export type GlobalSetStyle = BorderSetStyle &
+export type GlobalSetStyle = BorderStyle &
+  BorderRadiusStyle &
   BoxShadowStyle &
   ColorSetStyle &
+  OpacityStyle &
   SpaceSetStyle &
   SizeSetStyle &
   TextSetStyle &
   TransitionStyle
 
 export const globalSet = compose<GlobalSetProps, GlobalSetStyle>([
-  borderSet,
+  border,
+  borderRadius,
   boxShadow,
   colorSet,
+  opacity,
   spaceSet,
   sizeSet,
   textSet,
